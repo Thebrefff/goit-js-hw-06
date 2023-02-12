4922cf661e266e3ce2e5fc28c4f994c0763cd84b
Thebrefff <filip_zielinski@o2.pl>
2023-02-12 16:02:30 +0100

    12.02

--- task-01.html/task-01.js
+++ task-01.html/task-01.js
@@ -1,10 +1,11 @@
-const categoriesList = document.querySelectorAll('li.item');
+const categoriesList = document.querySelectorAll('.item');
 console.log(`Number of categories: ${categoriesList.length}`);
 
 categoriesList.forEach(item => {
     console.log(`Category: ${item.firstElementChild.textContent}`);
-    console.log(`Elements: ${item.lastElementChild.children.length}`);   
+    console.log(`Elements: ${item.lastElementChild.children.length}`);
 });
+
 
 
 
