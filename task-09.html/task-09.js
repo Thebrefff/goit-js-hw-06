f7c5c458a23b494843c8f8b83a61a01d6cdbf8b3
Thebrefff <filip_zielinski@o2.pl>
2023-02-19 18:37:01 +0100

    19/02

--- task-09.html/task-09.js
+++ task-09.html/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+const bgColor = document.querySelector('body');
+const colorName = document.querySelector('.color');
+const colorChanger = document.querySelector('.change-color');
+
+colorChanger.addEventListener('click', e => {
+  let color = getRandomHexColor();
+  colorName.textContent = color;
+  bgColor.style.backgroundColor = color;
+});
