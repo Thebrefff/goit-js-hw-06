d5a88a091a982aed4636687764d10c3c41ae3008
Thebrefff <filip_zielinski@o2.pl>
2023-02-19 19:28:36 +0100

    19/02 4.0

--- task-08.html/task-08.js
+++ task-08.html/task-08.js
@@ -1,20 +1,17 @@
 const loginForm = document.querySelector('.login-form');
 
 loginForm.addEventListener("submit", (event) => {
+		event.preventDefault();
 
-	event.preventDefault();
+		const {
+			elements: { email, password },
+		} = event.currentTarget;
 
-    const {
-    elements: { email, password },
-  } = event.currentTarget;
-
-if (email.value === '' || password.value === '') {
-alert("All fields should be filled!");
-}
-  const userDetails = { email: email.value, Password: password.value }    
-    console.log(userDetails)
-    event.currentTarget.reset()
-});
-
-
-
+		if (email.value === "" || password.value === "") {
+			alert("All fields should be filled!");
+		} else {
+			const userDetails = { email: email.value, Password: password.value };
+			console.log(userDetails);
+			event.currentTarget.reset();
+		}
+	});
